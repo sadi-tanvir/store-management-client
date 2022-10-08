5137120e948cd2566637ce70d718c2b02a1da232
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-10-08 10:54:14 +0600

    initial commit

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-primary font-bold'>
+                welcome to home page
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#4ade80",
+          secondary: "#f87171",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+}
